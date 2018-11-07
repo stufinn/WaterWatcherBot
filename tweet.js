1317a50f544f1b3abca7e3ef5813a122c59a7ba8
stufinn <30563561+finst@users.noreply.github.com>
2018-11-07 10:03:14 -0600

    Set up tweet module and setInterval() timer

---
+++ tweet.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const Twitter = require('twitter');
+const sortData = require('./sortData');
+
+const twitter = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+
+const tweetActive = (data) => {
+
+    var tweet = sortData.randomActive(data);
+
+    var tweetParameters = {
+        status: tweet
+    };
+
+    twitter.post('statuses/update',tweetParameters, (err,response)=> {
+        if (err) {
+            console.log('Something went wrong:', err);
+        } else {
+            console.log('Tweet posted');
+            // console.log(response);
+        }
+    });
+};
+
+module.exports = {
+    tweetActive
+};
