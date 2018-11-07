0e83e6fa53c734c00788af8f35391c81eea329f1
stufinn <30563561+finst@users.noreply.github.com>
2018-11-07 12:05:21 -0600

    Add node-schedule module for each day

--- server.js
+++ server.js
@@ -1,30 +1,22 @@
 const express = require('express');
 const fs = require('fs');
 const appFile = require('./app');
+const schedule = require('node-schedule');
 //defaults to 3000 if PORT doesn't exist on process.env
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-setInterval( () => {
-
-    app.use ( (req, res, next) => {
-        var now = new Date().toString();
-
-        var log = `${now}: ${req.method} ${req.url}`;
-        console.log(log);
-        fs.appendFile('server.log', log + '\n', (err) => {
-            if (err) {
-                console.log('Unable to append to server.log.');
-            }
-        });
-
-        next();  //must be called or else the middleware will call the application to hang
-    });
-
+var schedule = ('* 10 12 * * *', () => {
 
     appFile.postTweets();
-}, 30000);
+
+});
+
+// setInterval( () => {
+
+//      appFile.postTweets();
+// }, 30000);
 
 
 app.listen(port, () => {  //see port variable defined near top of app
