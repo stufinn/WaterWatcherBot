229555fcf6c717f4ed9206e1c46a15993815e8a1
stufinn <30563561+finst@users.noreply.github.com>
2018-11-07 12:22:43 -0600

    Fix timer

--- server.js
+++ server.js
@@ -2,22 +2,16 @@ const express = require('express');
 const fs = require('fs');
 const appFile = require('./app');
 const schedule = require('node-schedule');
+
 //defaults to 3000 if PORT doesn't exist on process.env
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-var schedule = ('* 10 12 * * *', () => {
-
+//see https://www.npmjs.com/package/node-schedule for more information
+var j = schedule.scheduleJob('30 12 * * *', () => {
     appFile.postTweets();
-
 });
-
-// setInterval( () => {
-
-//      appFile.postTweets();
-// }, 30000);
-
 
 app.listen(port, () => {  //see port variable defined near top of app
     console.log(`Server is up and running on port ${port}`);
